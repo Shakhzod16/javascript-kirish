47ade5e74f7e4f301d3d0cfb2fc6ac504d8a1e8a
shahzod88888 <islomovdilbek01@gmail.com>
2025-04-07 10:30:01 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,238 @@
+alert("hello javascript");
+console.log(1);
+console.log(2);
+
+// =========== lesson
+
+console.log("Shahzod");
+let ism = "shahzod";
+let yosh = 20;
+console.log(ism, yosh);
+
+yosh = 21;
+
+console.log(yosh);
+
+const familiya = "Qosimov";
+// familiya = 'Qoziyev'
+console.log(familiya);
+
+// =============== lesson
+
+var joy = "Buxoro";
+// var eskirgan hisoblanadi
+joy = "Vobken tumani";
+console.log(joy);
+
+// =============  lesson string
+
+console.log("hello world");
+
+// let email = "qosimov0805@gmail.com";
+// console.log(email);
+
+// string concstenation
+
+let firstName = "Shahzod";
+let lastName = "Qosimov";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[5]);
+
+// string length => uzunlik
+console.log(fullName.length);
+
+// string methods => funksiyaga o'xshash ish bajaradi
+console.log(fullName.toUpperCase());
+
+// let result = fullName.toLowerCase()
+// console.log(result)
+
+// let index = email.indexOf("@");
+// console.log(index);
+
+// let index = firstName.indexOf('f')
+// console.log(index)
+
+// lesson String Methods
+
+// let email = 'qosimov0805@gmail.com'
+
+// lastIndexOf()
+
+// indexof
+// let result = email.indexOf(0)
+// console.log(result)
+
+// slice()
+
+// let result = email.slice(0, 1);
+// console.log(result);
+
+// let result = email[0];
+// console.log(result);
+
+// sunstr()
+
+// let result = email.substr(0, 1)
+// console.log(result)
+
+// replace()
+
+// let res = email.replace('a', 'd')
+// console.log(res)
+
+// charAt birinchi harakterni olib beradi
+
+// let res = email.charAt();
+// console.log(res);
+
+// toUpperCase()-toLowerCase()
+
+// let nat = email.toUpperCase();
+// console.log(nat);
+
+// trim()
+
+// console.log(email)
+
+// let resum = email.trim();
+// console.log(resum.length, email.length);
+
+// splice()
+
+// let rem = email.split("");
+// console.log(rem);
+
+// lesson
+
+let radius = 10;
+const p = 3.14;
+
+// console.log(radius, p);
+
+// math operators +,-,*,/,%
+
+// console.log(10 / 2)
+// console.log(10 * 2)
+
+// let results = radius % 3
+// console.log(results)
+
+// let results = p * radius ** 2
+// console.log(results)
+
+// order of operation
+
+// let natija = 5 * (10 - 3) ** 2;
+
+// let like = 10;
+
+// like = like + 1;
+// like++
+
+// like += 10
+// like -= 10
+// like /= 10
+
+// console.log(like);
+
+// NaN  - not a number
+
+// console.log(5 / "salom");
+
+// let results = 'the blog has' + like + 'like';
+// console.log(results)
+
+// LESSON-12
+
+// const title = "Rastamojka qachon ochiladi ? yohud Monopoliya Respublika";
+// const author = "Lag'mon markazi";
+// const likes = 350000000;
+
+// concstentation way
+
+// let natija =
+//     "Bu post nomi " +
+//     " " +
+//     author +
+//     " tomonida yozilgan. " +
+//     "va bu postda" +
+//     likes +
+//     " ta like bor";
+// console.log(natija);
+
+// template string way
+
+// let natija = 'bu post nomi ${title}, ${author}, tomonida yozilga. Va bu postda ${likes} ta like bor'
+
+// let natija = "<h2>${title}</h2>";
+
+// console.log(natija);
+
+// LESSON-13
+
+// const mehmon1 = "aziz";
+// const mehmon2 = "sanjar";
+// const mehmon3 = "sherzod";
+// const mehmon4 = "bekzod";
+
+// array => massiv
+
+// const mehmonlar = ["aziz", "sanjar", "sherzod", "bekzod"];
+
+// mehmonlar[3] = "sanjar";
+
+// console.log(mehmonlar);
+
+// let number = [1, 2, 3, 4];
+// console.log(number);
+
+// property
+
+// const natija = (number = number.length);
+// let natija = mehmonlar.concat(number);
+// mehmonlar.push("alibek");
+
+// mehmonlar.pop("aziz");
+
+// console.log(natija);
+
+// LESSON-14
+
+// let age = null;
+// console.log(age, age + 3, "my name is ${age}");
+
+// LESSON-15
+
+// booleans & comporison
+
+console.log(true, false, "true", "false");
+
+// methods can return booleans
+
+let email = "qosimov0809@gmaiol.com";
+let natija = email.includes("@");
+
+// console.log(email)
+
+// comparison operators
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 40);
+
+// console.log(age !== 30) ! bu flaseni truega aylantiradi va aksi
+
+// LESSON-16
+let age = 19;
+
+// kuchlisiz taqoslashlar
+
+// ================
+
+
