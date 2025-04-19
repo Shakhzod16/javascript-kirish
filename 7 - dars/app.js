adc3805077b2ed242aa6130b5fb07c87687afabd
shahzod88888 <islomovdilbek01@gmail.com>
2025-04-19 11:08:33 +0500

    new lesson

--- 7 - dars/app.js
+++ 7 - dars/app.js
@@ -103,3 +103,131 @@
 // }
 
 // ===== 8 - masala =====
+
+// let Books = [
+// 	{ sarlavha: 'molxona', muallif: 'jorge orevel', isAvailable: 'true' },
+// 	{ sarlavha: 'toglar ham sado berdi', muallif: 'jorge orevel', isAvailable: 'true' },
+// 	{ sarlavha: 'ufq', muallif: 'jorge orevel', isAvailable: 'false' },
+// 	{ sarlavha: 'molxona', muallif: 'jorge orevel', isAvailable: 'true' },
+// ];
+
+// for (let i of Books) {
+// 	if (i.isAvailable === true) {
+// 		console.log(`${i.sarlavha} — ${i.muallif}`);
+// 	}
+// }
+
+// ===== masala =====
+
+// let Books = [
+// 	{ sarlavha: 'molxona', muallif: 'jorge orevel', isAvailable: 'true' },
+// 	{ sarlavha: 'toglar ham sado berdi', muallif: 'jorge orevel', isAvailable: 'true' },
+// 	{ sarlavha: 'ufq', muallif: 'jorge orevel', isAvailable: 'false' },
+// 	{ sarlavha: 'molxona', muallif: 'jorge orevel', isAvailable: 'true' },
+// ];
+
+// for (let i of Books) {
+// 	if (i.isAvailable === 'true') {
+// 		console.log(`${i.sarlavha} — ${i.muallif}`);
+// 	}
+// }
+
+// ===== 9 - masala =====
+
+// function getInfo() {
+// 	let foydalanuvchi = [{ nomi: 'Ali', obunachilar_soni: 250000, postlar_soni: 22340 }];
+// 	console.log(Object.values(foydalanuvchi));
+// }
+// getInfo();
+
+// ===== 10 - masala =====
+
+// let sportchi = [
+// 	{ name: 'baxodir', sport_turi: 'box', ball: '89' },
+// 	{ name: 'ruslan', sport_turi: 'box', ball: '39' },
+// 	{ name: 'shaxram', sport_turi: 'box', ball: '79' },
+// 	{ name: 'hasanboy', sport_turi: 'box', ball: '90' },
+// 	{ name: 'abduqodir', sport_turi: 'box', ball: '82' },
+// 	{ name: 'maxmud', sport_turi: 'box', ball: '80' },
+// ];
+
+// for (let i of sportchi) {
+// 	if (i.ball > 80) console.log(i);
+// }
+
+// ===== 11 - masala =====
+
+// Homework
+
+// let ishchilar = [
+// 	{ ism: 'sobir', lavozimi: 'suvoqchi', maosh: '9000' },
+// 	{ ism: 'firdavs', lavozimi: 'boshliq', maosh: '11000' },
+// 	{ ism: 'oktam', lavozimi: 'ish boshqaruvchi', maosh: '2000' },
+// 	{ ism: 'ilyos', lavozimi: 'suvoqchi', maosh: '90000' },
+// ];
+
+// for (let i of ishchilar) {
+// 	console.log(`${i.ism} ,${i.maosh} so'm`);
+// }
+
+// ===== 12 - masala =====
+
+// let products = {
+// 	phone: 1200,
+// 	laptop: 2500,
+// 	monitor: 800,
+// 	keyboard: 300,
+// };
+// let engqimmat = [];
+// for (let i of products) {
+//   if (0 > i)
+//     console.log(`$engqimmat`)
+// }
+
+// ===== masala =====
+
+// let products = {
+// 	phone: 1200,
+// 	laptop: 2500,
+// 	monitor: 800,
+// 	keyboard: 300,
+// };
+
+// let a = Object.values(products);
+
+// let maxc = a[0];
+
+// for (let i = 1; i < a.length; i++) {
+// 	if (maxc < a[i]) {
+// 		maxc = a[i];
+// 	}
+// }
+// console.log(maxc);
+
+// ===== 13 - masala =====
+
+// let c = 0;
+// let grades = [5, 4, 5, 3, 2];
+// let student = {
+// 	grades: grades,
+// };
+// for (let i of student.grades) {
+// 	c += i;
+// }
+// console.log(c / grades.length);
+
+// ===== 14 - masala =====
+
+// let xodimlar = [
+// 	{ name: 'islom', lavozimi: 'manager' },
+// 	{ name: 'dilbek', lavozimi: 'manager' },
+// 	{ name: 'shoxrux', lavozimi: 'ishchi' },
+// 	{ name: 'ozod', lavozimi: 'derektor' },
+// ];
+// for (let i of xodimlar) {
+//   if (i.lavozimi == 'manager')
+//     console.log(i)
+// }
+
+// ===== 16 - masala =====
+
