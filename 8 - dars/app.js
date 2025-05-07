8001cb6ed36db9588d2fd3d6b452cfe498ad17ac
shahzod88888 <islomovdilbek01@gmail.com>
2025-05-07 22:20:11 +0500

    new lesson

--- 8 - dars/app.js
+++ 8 - dars/app.js
@@ -36,27 +36,58 @@
 
 
 
-let numbers = [11, 2, 33, 33, 4];
+// let numbers = [11, 2, 33, 33, 4];
 
-let removeDuplicates = new Function("arr", `
-  let counts = {}; 
-  let result = [];  
-  
- 
-  for (let num of arr) {
-    counts[num] = (counts[num] || 0) + 1; 
-  }
+// let removeDuplicates = new Function("arr", `
+//   let counts = {};
+//   let result = [];
 
-  
-  for (let num in counts) {
-    if (counts[num] !== 2) {  
-      result.push(parseInt(num)); 
-    }
-  }
-  
-  return result;
-`);
 
-let result = removeDuplicates(numbers);
-console.log(result);
+//   for (let num of arr) {
+//     counts[num] = (counts[num] || 0) + 1;
+//   }
 
+
+//   for (let num in counts) {
+//     if (counts[num] !== 2) {
+//       result.push(parseInt(num));
+//     }
+//   }
+
+//   return result;
+// `);
+
+// let result = removeDuplicates(numbers);
+// console.log(result);
+
+// Rekursiv - fuksiya
+
+// ===== 1 - masala =====
+
+// function pow(x, n) {
+//     if (n == 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1)
+//     }
+// }
+// console.log(pow(2, 3))
+
+// ===== 2 - masala =====
+
+// function fibonacciNumber(n) {
+//     let a = 0, b = 1;
+
+//     for (let i = 2; i <= n; i++) {
+//         let next = a + b;
+//         a = b;
+//         b = next;
+//     }
+
+//     return b;
+// }
+
+// console.log(fibonacciNumber(100000));
+
+
+// =============
